4a1f73f84c6518197edeae7b25e8cc25b6c25b72
Nelo Puchades <correo@nelo.dev>
2021-09-16 23:09:47 +0200

    Playing with lists

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ int main() {
     int edad = 25;
     edad = 18;
     const char letra = 'A';
+    int lista_edades[] = {18, 24, 16};
+    std::cout << lista_edades[0] << std::endl;
+    std::cout << lista_edades[1] << std::endl;
+    std::cout << lista_edades[2] << std::endl;
     std::cout << edad << std::endl;
     std::cout << letra << std::endl;
 } 
