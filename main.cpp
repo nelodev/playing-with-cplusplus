57b57e869803335d3748beb40b78ebda2ed33c38
Nelo Puchades <correo@nelo.dev>
2021-09-16 23:10:34 +0200

    Rewriting item of list

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     edad = 18;
     const char letra = 'A';
     int lista_edades[] = {18, 24, 16};
+    lista_edades[2] = 32;
     std::cout << lista_edades[0] << std::endl;
     std::cout << lista_edades[1] << std::endl;
     std::cout << lista_edades[2] << std::endl;
